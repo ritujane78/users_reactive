38586612bf563d0c118cf27f4951c09d2f2c513e
Ritu Bafna <ritujane78@gmail.com>
2026-03-13 17:20:25 +0545

    add security

---
+++ src/main/java/com/jane/reactive/ws/users/infrastructure/WebSecurity.java
@@ -0,0 +1,27 @@
+package com.jane.reactive.ws.users.infrastructure;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class WebSecurity {
+
+
+    @Bean
+    public SecurityWebFilterChain httpSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+        return http
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers(HttpMethod.POST, "/users").permitAll()
+                        .anyExchange().authenticated())
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+}
