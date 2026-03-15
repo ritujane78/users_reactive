4dad1bdebe5ea907d94fbf2fdfaed10a8b3237a4
Ritu Bafna <ritujane78@gmail.com>
2026-03-15 13:58:38 +0545

    perform method level security: postAuthorize

--- src/main/java/com/jane/reactive/ws/users/presentation/UsersController.java
+++ src/main/java/com/jane/reactive/ws/users/presentation/UsersController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -32,7 +33,8 @@ public class UsersController {
     }
 
     @GetMapping("/{userId}")
-    @PreAuthorize("authentication.principal.equals(#userId.toString()) or hasRole('ROLE_ADMIN ')")
+//    @PreAuthorize("authentication.principal.equals(#userId.toString()) or hasRole('ROLE_ADMIN ')")
+    @PostAuthorize("returnObject.body !=null and (returnObject.body.id.toString().equals(authentication.principal))")
     public Mono<ResponseEntity<UserRest>> getUser(@PathVariable("userId") UUID userId) {
         return userService.getUserById(userId)
                 .mapNotNull(item -> ResponseEntity.status(HttpStatus.OK).body(item))
